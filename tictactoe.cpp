db1ecd4f5288cce175e379f994829db80a756907
russell-chai <russell.chai@hotmail.com>
2017-09-27 10:33:51 -0700

    update tictactoe

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -64,65 +64,65 @@ int main() {
    int XWins = 0;//number of times x was placed
    int YWins = 0;//number of times y was placed
    while(true) {
-   cout << "Enter move by row, column (ex. a1)" << endl;
-   counter = 0;
-   for (int a = 0; a < 3; a++) {
-     for (int b = 0; b < 3; b++) {
-       board[a][b] = ' ';
-     }
-   }
-   printBoard(board);
-   while(true) {
-     //get input
-    char input[2];
-    cin >> input;
-    int x = input[0] - 'a';
-    int y = input[1] - '1';
-    //check if theres already a piece there
-    if (board[x][y] == ' ') {
-      //check which player's turn
-      if (counter % 2 == 0) {
-	board[x][y] = 'X';
-	if (checkWin('X', board)) { //if player X won...
-	  XWins++;
-	  printBoard(board);
-	  cout << "X Wins" << endl;
-	  cout << "games won" << endl;
-	  cout << "x: " << XWins << " games" << endl;
-	  cout << "y: " << YWins << " games" << endl;
-	  counter = 0;
-          for (int a = 0; a < 3; a++) {
-             for (int b = 0; b < 3; b++) {
-                 board[a][b] = ' ';
-             }
-          }
-	  break;
-	}
+      cout << "Enter move by row, column (ex. a1)" << endl;
+      counter = 0;
+      for (int a = 0; a < 3; a++) {
+          for (int b = 0; b < 3; b++) {
+	      board[a][b] = ' ';
+	  }
       }
-      else {
-	board[x][y] = 'O';
-	if (checkWin('O', board)) {//if player O won...
-	  YWins++;
-	  printBoard(board);
-	  cout << "O Wins" << endl;
-	  cout << "games won" << endl;
-	  cout << "x: " << XWins << " games" << endl;
-	  cout << "y: " << YWins << " games" << endl;
-	  counter = 0;
-          for (int a = 0; a < 3; a++) {
-              for (int b = 0; b < 3; b++) {
-                 board[a][b] = ' ';
-              }
-          }
-	  break;
-	}
-      }
-      counter++;
       printBoard(board);
-    }
-    else {
-      cout << "invalid move" << endl;
-    }
-  }
-}
+      while(true) {
+          //get input
+	  char input[2];
+	  cin >> input;
+	  int x = input[0] - 'a';
+	  int y = input[1] - '1';
+	  //check if theres already a piece there
+	  if (board[x][y] == ' ') {
+	      //check which player's turn
+	      if (counter % 2 == 0) {
+	          board[x][y] = 'X';
+		  if (checkWin('X', board)) { //if player X won...
+		      XWins++;
+		      printBoard(board);
+		      cout << "X Wins" << endl;
+		      cout << "games won" << endl;
+		      cout << "x: " << XWins << " games" << endl;
+		      cout << "y: " << YWins << " games" << endl;
+		      counter = 0;
+		      for (int a = 0; a < 3; a++) {
+		        for (int b = 0; b < 3; b++) {
+			   board[a][b] = ' ';
+		        }
+		      }
+		      break;
+		  }
+	      }
+	      else {
+	          board[x][y] = 'O';
+		  if (checkWin('O', board)) {//if player O won...
+		      YWins++;
+		      printBoard(board);
+		      cout << "O Wins" << endl;
+		      cout << "games won" << endl;
+		      cout << "x: " << XWins << " games" << endl;
+		      cout << "y: " << YWins << " games" << endl;
+		      counter = 0;
+		      for (int a = 0; a < 3; a++) {
+		         for (int b = 0; b < 3; b++) {
+			     board[a][b] = ' ';
+		         }
+		      }
+		      break;
+		   }
+	       }
+	       counter++;
+	       printBoard(board);
+	   }
+	   else {
+	       cout << "invalid move" << endl;
+	   }
+       }
+   }   
 }
