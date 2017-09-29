4044fe040f60cd4d7619d6a1355ceb696ba1d8c7
U-BSD\417856 <417856@SHS-10DCE0ADE.beaverton.k12.or.us>
2017-09-29 12:19:52 -0700

    edit

--- StudentList.cpp
+++ StudentList.cpp
@@ -17,54 +17,53 @@ struct student {
   float GPA;
 };
 //add student
-void addStudent(vector<student> &students) {
-  student temp;
+void addStudent(vector<student*>* students) {
+  student* temp = new student();
   cout << "what is student's first name" << endl;
-  cin >> temp.firstName;
+  cin >> temp->firstName;
   cout << "what is student's last name" << endl;
-  cin >> temp.lastName;
+  cin >> temp->lastName;
   cout << "what is student's ID" << endl;
-  cin >> temp.ID;
+  cin >> temp->ID;
   cout << "what is student's GPA" << endl;
-  cin >> temp.GPA;
+  cin >> temp->GPA;
   //add student to vector
-  students.push_back(temp);
+  students->push_back(temp);
 }
 //print all the students
-void printStudents(vector<student> &students) {
+void printStudents(vector<student*>* students) {
   cout << setprecision(2) << fixed;
-  for (int a = 0; a < students.size(); a++) {
-    cout << students[a].firstName << " " << students[a].lastName << ", " <<
-      students[a].ID << ", " << students[a].GPA << endl;
+  for (int a = 0; a < students->size(); a++) {
+    cout << students->at(a)->firstName << " " << students->at(a)->lastName << ", " <<
+      students->at(a)->ID << ", " << students->at(a)->GPA << endl;
   }
 }
 //delete students
-void deleteStudents(vector<student> &students) {
+void deleteStudents(vector<student*>* students) {
   int tempId;
   cout << "what is student's ID" << endl;
   cin >> tempId;
-  for (int a = 0; a < students.size(); a++) {
-    if (students[a].ID == tempId) {
-      students.erase(students.begin() + a);
+  for (int a = 0; a < students->size(); a++) {
+    if (students->at(a)->ID == tempId) {
+      students->erase(students->begin() + a);
       break;
     }
   }
 }
 int main() {
-  vector<student> students;
-  vector<student> *ptrStudent = &students;
+  vector<student*> *ptrStudent = new vector<student*>();
   while (true) {
     char *input;
     cin >> input;
     //takes in input and check for command
     if (strcmp(input, "ADD") == 0) {
-      addStudent(students);
+      addStudent(ptrStudent);
     }
     else if (strcmp(input, "PRINT") == 0) {
-      printStudents(students);
+      printStudents(ptrStudent);
     }
     else if (strcmp(input, "DELETE") == 0) {
-      deleteStudents(students);
+      deleteStudents(ptrStudent);
     }
     else if (strcmp(input, "QUIT") == 0) {
       break;
